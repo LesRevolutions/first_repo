ab9c15b8ab5da955d638613485f2e2b288b14e65
Devito K.S. <dkstevanus@gmail.com>
2025-06-03 08:08:24 -0400

    Add the plots depicting the OPS+ of qualified batters in the 2000-2009 and 2010-2019 eras (+ the R code for both plots)

--- OPS eras.R
+++ OPS eras.R
@@ -21,3 +21,8 @@ str(war_daily$year_ID)
 # Step 1: Separate the 2010s era from the main tibble
 war_2010s <- war_daily %>% dplyr::filter(year_ID >= 2010 & year_ID <= 2019 & PA >= 502)
 war_2010s
+war_2010s$OPS_plus <- as.numeric(as.character(war_2010s$OPS_plus))
+
+base2010s <- ggplot(war_2010s, aes(x = year_ID, y = OPS_plus)) + geom_point() + scale_x_continuous(n.breaks = 9, "Season") + scale_y_continuous("OPS+")
+plot2010s <- base2010s + theme_classic()
+plot2010s
