26147a4393283cb33f9900f189b76b972cced0f3
Devito K.S. <dkstevanus@gmail.com>
2025-06-03 07:46:52 -0400

    Clean the PA columns from any nulls and convert it from chars to num

--- OPS eras.R
+++ OPS eras.R
@@ -7,3 +7,17 @@ library(ggplot2)
 
 war_daily <- read_xlsx("C:/Users/Stevanus/Downloads/war_daily_bat.xlsx")
 war_daily
+
+# Clear the rows whose Plate Appearances section is NULL and convert from chars to num
+war_daily <- na.omit(war_daily, cols = 'PA')
+war_daily$PA <- as.numeric(as.character(war_daily$PA))
+
+# Check data type
+str(war_daily$PA)
+str(war_daily$year_ID)
+
+
+# FIRST ERA: 2010-2019
+# Step 1: Separate the 2010s era from the main tibble
+war_2010s <- war_daily %>% dplyr::filter(year_ID >= 2010 & year_ID <= 2019 & PA >= 502)
+war_2010s
