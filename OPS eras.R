3eb73eb320ed0dfdc145ed59d63273f4ad885d61
Devito K.S. <dkstevanus@gmail.com>
2025-06-03 15:54:47 -0400

    Added the 1960s, 1970s, and 1980s eras code in R and its plots

--- OPS eras.R
+++ OPS eras.R
@@ -43,7 +43,7 @@ plot2000s <- base2000s + theme_classic()
 plot2000s
 
 
-# SECOND ERA: 1990-1999
+# THIRD ERA: 1990-1999
 # Step 2: Extract the 1990s era from the main tibble
 war_1990s <- war_daily %>% dplyr::filter(year_ID >= 1990 & year_ID <= 1999 & PA >= 502)
 war_1990s
@@ -54,3 +54,40 @@ base1990s <- ggplot(war_1990s, aes(x = year_ID, y = OPS_plus)) + geom_point() +
   scale_x_continuous(n.breaks = 9, name = "Season") + scale_y_continuous("OPS+", limits = c(0, 300))
 plot1990s <- base1990s + theme_classic()
 plot1990s
+
+
+
+
+
+
+# REMAINING ERAS: 1980s, 1970s, 1960s
+# 1980-1989
+war_1980s <- war_daily %>% dplyr::filter(year_ID >= 1980 & year_ID <= 1989 & PA >= 502)
+war_1980s$OPS_plus <- as.numeric(as.character(war_1980s$OPS_plus))
+
+base1980s <- ggplot(war_1980s, aes(x = year_ID, y = OPS_plus)) + geom_point() + 
+  scale_x_continuous(n.breaks = 9, name = "Season") + scale_y_continuous("OPS+", limits = c(0, 300))
+plot1980s <- base1980s + theme_classic()
+plot1980s
+
+
+
+# 1970-1979
+war_1970s <- war_daily %>% dplyr::filter(year_ID >= 1970 & year_ID <= 1979 & PA >= 502)
+war_1970s$OPS_plus <- as.numeric(as.character(war_1970s$OPS_plus))
+
+base1970s <- ggplot(war_1970s, aes(x = year_ID, y = OPS_plus)) + geom_point() + 
+  scale_x_continuous(n.breaks = 9, name = "Season") + scale_y_continuous("OPS+", limits = c(0, 300))
+plot1970s <- base1970s + theme_classic()
+plot1970s
+
+
+
+# 1960-1969
+war_1960s <- war_daily %>% dplyr::filter(year_ID >= 1960 & year_ID <= 1969 & PA >= 502)
+war_1960s$OPS_plus <- as.numeric(as.character(war_1960s$OPS_plus))
+
+base1960s <- ggplot(war_1960s, aes(x = year_ID, y = OPS_plus)) + geom_point() + 
+  scale_x_continuous(n.breaks = 9, name = "Season") + scale_y_continuous("OPS+", limits = c(0, 300))
+plot1960s <- base1960s + theme_classic()
+plot1960s
