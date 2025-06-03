cd83ae3520af53e222b62d0c1bf021fa3b3d0adf
Devito K.S. <dkstevanus@gmail.com>
2025-06-03 07:14:34 -0400

    Added the Excel file containing all MLB players' statistics + the R file

---
+++ OPS eras.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(magrittr)
+library(dplyr)
+library(readxl)
+library(writexl)
+library(ggplot2)
+
+war_daily <- read_xlsx("C:/Users/Stevanus/Downloads/war_daily_bat.xlsx")
+war_daily
